eb42fd37a534b2f4a9c35ce82176f646e62b4123
Abdulrahman <abdul.saif.hadhrami@gmail.com>
2019-03-20 11:39:29 -0500

    Predict.py initialization
    The main structure of the prediction script is created

---
+++ predict.py
@@ -0,0 +1,22 @@
+def predict(image_path, model, topk=3):
+    ''' Predict the class (or classes) of an image using a trained deep learning model.
+    '''
+    # TODO: Implement the code to predict the class from an image file
+    # process the image so that it can be fit in the model
+    image = process_image(image_path)
+    image = torch.from_numpy(image).type(torch.FloatTensor).unsqueeze_(0)
+    # run the image in the model to get log of probabilities
+    log_prob = model(image)
+    # convert the log into actual probabilities
+    prob = torch.exp(log_prob)
+    # get the top 5 probabilities
+    top_prob, top_lab = prob.topk(topk) # get the top 5 results
+    top_prob = top_prob.detach().numpy().tolist()[0]
+    top_lab = top_lab.detach().numpy().tolist()[0]
+
+    # Convert indices to classes
+    idx_to_class = {val: key for key, val in
+                                      model.class_to_idx.items()}
+    top_labels = [idx_to_class[lab] for lab in top_lab]
+    top_flowers = [cat_to_name[idx_to_class[lab]] for lab in top_lab]
+    return top_prob, top_labels, top_flowers
