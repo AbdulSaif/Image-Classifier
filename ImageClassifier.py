26679b8f37250ecbe32bdc6e015e7b78f40c9d4e
Abdulrahman <abdul.saif.hadhrami@gmail.com>
2019-03-19 19:37:19 -0500

    Update ImageClassifier.py
    1) Dataloaders are created
    2) Model is created
    3) Model is trained

--- ImageClassifier.py
+++ ImageClassifier.py
@@ -1,2 +1,8 @@
 # getting user inputs to required parameters and save them to a variable
 user_inputs = get_user_inputs()
+# creating train, validate and test dataloaders
+trainloader, validloader, testloader = create_dataloader(user_inputs.dir)
+# create the model to be trained
+model = create_model(user_inputs.arch, user_inputs.hidden_units)
+# training the model and save it as checkpoint
+train_model(trainloader, validloader, model, user_inputs.epochs, user_inputs.learning_rate, user_inputs.device)
