6dd5b60dde5273032eb460b359a7abc58bc8ab03
Abdulrahman <abdul.saif.hadhrami@gmail.com>
2019-03-20 11:35:02 -0500

    Create process_image.py
    This to process the image so that it can be fitted in the model i.e. image manipulations are applied (resize, crop and color channels edit etc..)

---
+++ process_image.py
@@ -0,0 +1,30 @@
+def process_image(image):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    # TODO: Process a PIL image for use in a PyTorch model
+    # importing required PIL and Image library
+    from PIL import Image as img
+    # open the image for processing
+    pp_image = img.open(image)
+    # resize the model so that shortest side is 256 pixels
+    if pp_image.size[0] < pp_image.size[1]:
+        pp_image.thumbnail((256, 5000))
+    else:
+        pp_image.thumbnail((5000, 256))
+    # cropping the image to be 224x224 pixels
+    l_margin = (pp_image.width - 224) / 2
+    r_margin = l_margin + 224
+    b_margin = (pp_image.width - 224) / 2
+    t_margin = b_margin + 224
+    pp_image = pp_image.crop((l_margin, b_margin, r_margin, t_margin))
+    # normalizing the image
+    pp_image = np.array(pp_image) / 255
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    p_image = (pp_image - mean) / std
+
+    # setting the color channel to be the first for pytorch
+    p_image = p_image.transpose((2, 0, 1))
+
+    return p_image
