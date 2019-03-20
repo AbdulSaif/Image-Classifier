1e309d21ba6c47df35c0eabf6a21cb08e14ba19c
Abdulrahman <abdul.saif.hadhrami@gmail.com>
2019-03-20 11:57:20 -0500

    ImageClassifier now specified for train user inputs

--- ImageClassifier.py
+++ ImageClassifier.py
@@ -1,8 +1,14 @@
 # getting user inputs to required parameters and save them to a variable
-user_inputs = get_user_inputs()
+t_user_inputs = get_t_user_inputs()
 # creating train, validate and test dataloaders
-trainloader, validloader, testloader = create_dataloader(user_inputs.dir)
+trainloader, validloader, testloader = create_dataloader(t_user_inputs.dir)
 # create the model to be trained
-model = create_model(user_inputs.arch, user_inputs.hidden_units)
+model = create_model(t_user_inputs.arch, t_user_inputs.hidden_units)
 # training the model and save it as checkpoint
-save_path = train_model(trainloader, validloader, model, user_inputs.epochs, user_inputs.learning_rate, user_inputs.device)
+train_model(trainloader, validloader, model, t_user_inputs.epochs, t_user_inputs.learning_rate, t_user_inputs.device)
+# loading the trained model to use it for production
+checkpoint = load_checkpoint(t_user_inputs.save_dir, t_user_inputs.arch, t_user_inputs.hidden_units, t_user_inputs.learning_rate):
+# set the model to evaluation mode
+checkpoint.eval()
+# predict class for single image
+predict(image_path, checkpoint)
