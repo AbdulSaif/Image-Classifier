8c387f2b2ba1481013bc340fc4ae6960a379ed81
Abdulrahman <abdul.saif.hadhrami@gmail.com>
2019-03-28 15:24:53 -0400

    Commented label_mapping.py

--- label_mapping.py
+++ label_mapping.py
@@ -1,7 +1,13 @@
+# This function takes a file that has labels and flowers name reads it and it
+# then returns it as dictionary containing labels as index and flowers name as
+# values
+
 def label_mapping(filename):
+    # importing required python module to read .json file
     import json
 
     with open(filename, 'r') as f:
-        cat_to_name = json.load(f)
-
-    return cat_to_name
+        label_to_name = json.load(f)
+    # returns dictionary containing labels as index and flowers name as values
+    # so that can be used by the main function
+    return label_to_name
