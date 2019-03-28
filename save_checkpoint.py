7464ac6146b8254bf871536ed129d8d6a6e732e9
Abdulrahman <abdul.saif.hadhrami@gmail.com>
2019-03-27 21:42:40 -0400

    Working save_checkpoint.py

--- save_checkpoint.py
+++ save_checkpoint.py
@@ -1,15 +1,12 @@
 # this function is to save a checkpoint for the trained model
-def checkpoint(save_dir):
-    save_path = save_dir + '/checkpoint.pth'
-    model.class_to_idx = train_datasets.class_to_idx
-    model.cpu()
-    checkpoint = {'arch': user_inputs.arch,
-                  'model_state_dict': model.state_dict(),
-                  'criterion_state_dict': criterion.state_dict(),
-                  'optimizer_state_dict': optimizer.state_dict(),
-                  'class_to_idx': model.class_to_idx,
-                  'epoch': epochs,
-                  'loss': loss}
-    torch.save(checkpoint, save_path)
-    print("The model has been saved")
-    return save_path
+def checkpoint(save_dir, train_datasets, trained_model, arch, epochs):
+    import torch
+    save_path = save_dir + 'checkpoint.pth'
+    trained_model.class_to_idx = train_datasets.class_to_idx
+    trained_model.cpu()
+    checkpoint = {'arch': arch,
+                  'model_state_dict': trained_model.state_dict(),
+                  'class_to_idx': trained_model.class_to_idx,
+                  'epoch': epochs}
+    saved_model = torch.save(checkpoint, save_path)
+    return saved_model
