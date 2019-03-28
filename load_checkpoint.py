16fb5dfd5666cd6d1bad801a4e8953d18aec32f2
Abdulrahman <abdul.saif.hadhrami@gmail.com>
2019-03-28 15:48:02 -0400

    Commented load_checkpoint.py

--- load_checkpoint.py
+++ load_checkpoint.py
@@ -1,19 +1,19 @@
-# TODO: Write a function that loads a checkpoint and rebuilds the model
+# This function loads a pretrained model for further future use either for
+# training or image classiifcation usage
 def load_checkpoint(filepath, arch, hidden_size, learning_rate):
-    # initializing the model, loss criterion and optimizer
+    # initializing the model with its CNN arch, hidden layer size etc..
     model = create_model(user_inputs.arch, user_inputs.hidden_units)
+
     # freezing model features and apply backpropagation to classifier only
     for param in model.parameters():
         param.requires_grad = False
-    criterion = nn.NLLLoss()
-    optimizer = optim.Adam(model.classifier.parameters(), lr = 0.001)
 
     # load the dictionary locally
     checkpoint = torch.load(filepath)
     model.load_state_dict(checkpoint['model_state_dict'])
-    criterion.load_state_dict(checkpoint['criterion_state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     epoch = checkpoint['epoch']
     loss = checkpoint['loss']
     model.class_to_idx = checkpoint['class_to_idx']
+
+    # returns the pretrained loaded model
     return model
