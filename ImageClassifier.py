9f6d130170fd22f2661cb92c35d7e69ca8b087c3
Abdulrahman <abdul.saif.hadhrami@gmail.com>
2019-03-27 21:49:14 -0400

    Update ImageClassifier.py
    1) Now all local variables from functions are saved as global variables in main
    2) Save function is now part of main and not train.py

--- ImageClassifier.py
+++ ImageClassifier.py
@@ -1,6 +1,6 @@
-from get_mode_user_inputs import get_mode_user_inputs
-from t_get_user_inputs import t_get_user_inputs
-from p_get_user_inputs import p_get_user_inputs
+from get_user_inputs import get_user_inputs
+#from t_get_user_inputs import t_get_user_inputs
+#from p_get_user_inputs import p_get_user_inputs
 from create_dataloader import create_dataloader
 from create_model import create_model
 from save_checkpoint import checkpoint
@@ -9,26 +9,29 @@ from load_checkpoint import load_checkpoint
 from process_image import process_image
 from predict import predict
 
+
 def main():
 # select which mode the user needs
-    mode = get_mode_user_inputs()
+    user_inputs = get_user_inputs()
     # getting user inputs to required parameters and save them to a variable
-    if mode == 'train':
-        user_inputs = t_get_user_inputs()
+    if user_inputs.mode == 'train':
         # creating train, validate and test dataloaders
-        trainloader, validloader, testloader = create_dataloader(user_inputs.dir)
+        train_datasets, valid_datasets, test_datasets, trainloader, validloader, testloader = create_dataloader(user_inputs.dir)
         # create the model to be trained
         model = create_model(user_inputs.arch, user_inputs.hidden_units)
         # training the model and save it as checkpoint
-        train_model(trainloader, validloader, model, user_inputs.epochs, user_inputs.learning_rate, user_inputs.device)
-#    else:
- #       user_inputs = p_get_user_inputs()
-  #      # loading the trained model to use it for production
-   #     checkpoint = load_checkpoint(user_inputs.checkpoint_path, user_inputs.arch, user_inputs.hidden_units, user_inputs.learning_rate)
-    #    # set the model to evaluation mode
-     #   checkpoint.eval()
-      #  # predict class for single image
-       # predict(user_inputs.image_path, user_inputs.checkpoint_path, user_inputs.category_names, user_inputs.device)
+        trained_model = train_model(train_datasets, trainloader, validloader, model, user_inputs.epochs, user_inputs.learning_rate, user_inputs.device, user_inputs.save_dir)
+        # save the model checkpoint
+        saved_model = checkpoint(user_inputs.save_dir, train_datasets, trained_model, user_inputs.arch, user_inputs.epochs)
+    else:
+        # loading the trained model to use it for production
+        model = create_model(user_inputs.arch, user_inputs.hidden_units)
+        load_checkpoint = load_checkpoint(user_inputs.checkpoint_path, model,
+        user_inputs.learning_rate)
+        # set the model to evaluation mode
+        checkpoint.eval()
+        # predict class for single image
+        predict(user_inputs.image_path, user_inputs.checkpoint_path, user_inputs.category_names, user_inputs.device)
 
 # Call to main function to run the program
 if __name__ == "__main__":
